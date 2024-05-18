e72ac39ea881c57ffd18bbec5adcb529c461edcf
Berkay Demir <berkayydmir@gmail.com>
2024-05-18 23:07:38 +0300

    feat : variables added

--- WeatherStation32/WeatherStation32.ino
+++ WeatherStation32/WeatherStation32.ino
@@ -1,4 +1,17 @@
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPL6radUUFbx"
 #define BLYNK_TEMPLATE_NAME "WeatherStation32"
-#define BLYNK_AUTH_TOKEN "mqxbXj-ho2-C25G-uZICtj62AgjdGd0W"
+
+char auth[] = "mqxbXj-ho2-C25G-uZICtj62AgjdGd0W";
+char ssid[] = "VodafoneNet-4d2a80";
+char pass[] = "8U5zBmMesJ"; 
+
+float temperature;
+float humidity;
+float pressure;
+float mbar;
+float uv;
+float visible;
+float ir;
+
+#define DHTPIN 5
