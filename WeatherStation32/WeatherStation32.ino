f1a59c2564f1f24be4d9d5e606f5ad0add1e505b
Berkay Demir <berkayydmir@gmail.com>
2024-05-26 14:18:40 +0300

    feat : create notifications and connecting to blynk

--- WeatherStation32/WeatherStation32.ino
+++ WeatherStation32/WeatherStation32.ino
@@ -1,6 +1,7 @@
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPL6radUUFbx"
 #define BLYNK_TEMPLATE_NAME "WeatherStation32"
+#define BLYNK_AUTH_TOKEN "mqxbXj-ho2-C25G-uZICtj62AgjdGd0W"
 
 #include <WiFi.h> // required libraries
 #include <WiFiClient.h>
@@ -11,9 +12,8 @@
 #include "BMP085.h"
 #include <Wire.h>
 
-char auth[] = "mqxbXj-ho2-C25G-uZICtj62AgjdGd0W";
-char ssid[] = "VodafoneNet-4d2a80";
-char pass[] = "8U5zBmMesJ"; 
+char ssid[] = "ssid";
+char pass[] = "password"; 
 
 float temperature;
 float humidity;
@@ -26,8 +26,9 @@ float ir;
 SI114X SI1145 = SI114X(); // sunlight sensor
 BMP085 myBarometer; // pressure sensor
 
-#define DHTPIN 5
+#define DHTPIN 4
 #define DHTTYPE DHT11
+#define LED_PIN 5
 
 DHT dht(DHTPIN, DHTTYPE); // dht sensor
 BlynkTimer timer;
@@ -48,12 +49,35 @@ void sendSensor()
   ir = SI1145.ReadIR(); // IR radiation
   uv = SI1145.ReadUV(); // UV index
 
+  // Convert UV to actual UV index
+  float uvIndex = uv / 100.0;
+
+  if (ir > 1500) 
+  {
+    Blynk.logEvent("ir_radiation");
+    digitalWrite(LED_PIN, HIGH); 
+  } 
+  else if (uvIndex > 7.9) 
+  {
+    Blynk.logEvent("uv_radiation");
+    digitalWrite(LED_PIN, HIGH); 
+  }
+  else if (temperature > 25)
+  {
+    Blynk.logEvent("high_temperature");
+    digitalWrite(LED_PIN, HIGH); 
+  }
+  else 
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+
   Blynk.virtualWrite(V0, temperature); 
-  Blynk.virtualWrite(V1, humidity);
+  //Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V1, uv);
   Blynk.virtualWrite(V2, mbar);
   Blynk.virtualWrite(V3, visible);
   Blynk.virtualWrite(V4, ir);
-  //Blynk.virtualWrite(V5, uv);
 }
 
 void setup()
@@ -68,14 +92,26 @@ void setup()
   Serial.println("Si1145 is ready!");
   myBarometer.init();
   dht.begin();
+  pinMode(LED_PIN, OUTPUT);
   delay(1000);
-  Blynk.begin(auth, ssid, pass);
-  delay(1000);
-  timer.setInterval(1000L, sendSensor); 
+
+  Serial.println("Connecting to Blynk...");
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  while (!Blynk.connected()) 
+  {
+    delay(1000);
+    Serial.println("Connecting to Blynk...");
+  }
+  Serial.println("Connected to Blynk");
+
+  timer.setInterval(1000L, sendSensor);
 }
 
 void loop() 
 {
-  Blynk.run()
-  timer.run()
+  sendSensor();
+  delay(1000);
+  Blynk.run();
+  timer.run();
 }
